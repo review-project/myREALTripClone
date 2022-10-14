efe8a1e56972f12d6c57b97dbf17fda3445fdee2
dotdotot <dotdotot203@naver.com>
2022-10-14 21:48:47 +0900

    ✨➕🔧 web project add

---
+++ web/js/imgSrc.js
@@ -0,0 +1,4 @@
+// db에 저장된 video src를 불러와서 변수로 저장하는 과정이 필요
+// video src변경 
+document.getElementById("img").src="imgFile/1.jpg";
+document.getElementById("img1").src="imgFile/2.jpg";
---
+++ web/js/videoSrc.js
@@ -0,0 +1,3 @@
+// db에 저장된 video src를 불러와서 변수로 저장하는 과정이 필요
+// video src변경 
+document.getElementById("video").src="videoFile/women.mp4";
